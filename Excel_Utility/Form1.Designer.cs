8c05f9601875f6dadbb09b4e52af50337d08e115
prathamesh_bhuvad <prathamesh_bhuvad@VASP-LAPTOP20>
2024-04-29 12:07:38 +0530

    Add project files.

---
+++ Excel_Utility/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Excel_Utility
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input_btn = new System.Windows.Forms.Button();
+            this.Output_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Process = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Success_txt = new System.Windows.Forms.TextBox();
+            this.Error_txt = new System.Windows.Forms.TextBox();
+            this.File_Name = new System.Windows.Forms.Label();
+            this.Folder_Name = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.reportViewer2 = new Microsoft.Reporting.WinForms.ReportViewer();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Input_btn
+            // 
+            this.Input_btn.Location = new System.Drawing.Point(53, 44);
+            this.Input_btn.Name = "Input_btn";
+            this.Input_btn.Size = new System.Drawing.Size(155, 23);
+            this.Input_btn.TabIndex = 0;
+            this.Input_btn.Text = "Select Input Excel File";
+            this.Input_btn.UseVisualStyleBackColor = true;
+            this.Input_btn.Click += new System.EventHandler(this.Input_btn_Click);
+            // 
+            // Output_btn
+            // 
+            this.Output_btn.Location = new System.Drawing.Point(53, 85);
+            this.Output_btn.Name = "Output_btn";
+            this.Output_btn.Size = new System.Drawing.Size(155, 23);
+            this.Output_btn.TabIndex = 1;
+            this.Output_btn.Text = "Select Output Folder";
+            this.Output_btn.UseVisualStyleBackColor = true;
+            this.Output_btn.Click += new System.EventHandler(this.Output_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Week Ending Date (YYYYMMDD)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(252, 150);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating_1);
+            // 
+            // Process
+            // 
+            this.Process.Location = new System.Drawing.Point(212, 197);
+            this.Process.Name = "Process";
+            this.Process.Size = new System.Drawing.Size(115, 23);
+            this.Process.TabIndex = 4;
+            this.Process.Text = "Process";
+            this.Process.UseVisualStyleBackColor = true;
+            this.Process.Click += new System.EventHandler(this.Process_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(195, 496);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 5;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(314, 496);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 6;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Success_txt
+            // 
+            this.Success_txt.Location = new System.Drawing.Point(63, 260);
+            this.Success_txt.Multiline = true;
+            this.Success_txt.Name = "Success_txt";
+            this.Success_txt.Size = new System.Drawing.Size(223, 80);
+            this.Success_txt.TabIndex = 7;
+            // 
+            // Error_txt
+            // 
+            this.Error_txt.Location = new System.Drawing.Point(314, 260);
+            this.Error_txt.Multiline = true;
+            this.Error_txt.Name = "Error_txt";
+            this.Error_txt.Size = new System.Drawing.Size(223, 32);
+            this.Error_txt.TabIndex = 8;
+            // 
+            // File_Name
+            // 
+            this.File_Name.AutoSize = true;
+            this.File_Name.Location = new System.Drawing.Point(354, 90);
+            this.File_Name.Name = "File_Name";
+            this.File_Name.Size = new System.Drawing.Size(35, 13);
+            this.File_Name.TabIndex = 9;
+            this.File_Name.Text = "label2";
+            // 
+            // Folder_Name
+            // 
+            this.Folder_Name.AutoSize = true;
+            this.Folder_Name.Location = new System.Drawing.Point(252, 90);
+            this.Folder_Name.Name = "Folder_Name";
+            this.Folder_Name.Size = new System.Drawing.Size(35, 13);
+            this.Folder_Name.TabIndex = 10;
+            this.Folder_Name.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Successfully Processed Job Nos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(389, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Errors / Issues";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(314, 313);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // reportViewer2
+            // 
+            this.reportViewer2.LocalReport.ReportEmbeddedResource = "Excel_Utility.rptJob.rdlc";
+            this.reportViewer2.Location = new System.Drawing.Point(642, 107);
+            this.reportViewer2.Name = "reportViewer2";
+            this.reportViewer2.ServerReport.BearerToken = null;
+            this.reportViewer2.Size = new System.Drawing.Size(396, 246);
+            this.reportViewer2.TabIndex = 14;
+            this.reportViewer2.Load += new System.EventHandler(this.reportViewer2_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 546);
+            this.Controls.Add(this.reportViewer2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Folder_Name);
+            this.Controls.Add(this.File_Name);
+            this.Controls.Add(this.Error_txt);
+            this.Controls.Add(this.Success_txt);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Process);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Output_btn);
+            this.Controls.Add(this.Input_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Input_btn;
+        private System.Windows.Forms.Button Output_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Process;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.TextBox Success_txt;
+        private System.Windows.Forms.TextBox Error_txt;
+        private System.Windows.Forms.Label File_Name;
+        private System.Windows.Forms.Label Folder_Name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer2;
+    }
+}
+
---
+++ Excel_Utility/Form1.cs
@@ -0,0 +1,196 @@
+﻿using ExcelDataReader;
+using Microsoft.Reporting.WebForms;
+using Microsoft.Reporting.WinForms;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Excel_Utility
+{
+    public partial class Form1 : Form
+    {
+        private string selectedFileName;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            File_Name.Visible = false;
+            Folder_Name.Visible = false;
+        }
+
+        public void Input_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Title = "Select a file";
+            openFileDialog.Filter = "Excel Files (*.xls;*.xlsx)|*.xls;*.xlsx";
+
+            
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedFileName = openFileDialog.FileName;
+                File_Name.Text = selectedFileName;
+                File_Name.Visible = true;
+            }
+        }
+
+        private void Output_btn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            folderBrowserDialog.Description = "Select a folder to save the file";
+
+            
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFolderPath = folderBrowserDialog.SelectedPath;
+                Folder_Name.Text = selectedFolderPath;
+                Folder_Name.Visible = true;
+
+            }
+        }
+        private void textBox1_Validating_1(object sender, CancelEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string input = textBox.Text.Trim();
+
+            if (!IsValidDate(input))
+            {
+                MessageBox.Show("Invalid date format. Please enter a date in the format YYYYMMDD.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Focus();
+                e.Cancel = true;
+            }
+        }
+        private bool IsValidDate(string input)
+        {
+            // Check if the input string has 8 characters
+            if (input.Length != 8)
+                return false;
+
+            // Check if all characters are digits
+            if (!input.All(char.IsDigit))
+                return false;
+
+            // Parse the input string as a date
+            if (!DateTime.TryParseExact(input, "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out _))
+                return false;
+
+            return true;
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            File_Name.Text = "";
+            Folder_Name.Text = "";
+            textBox1.Text = "";
+            Success_txt.Text = "";
+            Error_txt.Text = "";
+        }
+       
+        private void Process_Click(object sender, EventArgs e)
+        {
+
+            string filePath = selectedFileName;
+
+
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                
+                DataSet1 dataSet = new DataSet1();
+                int sheetIndex = 0; // Index of the worksheet to read, 0 for the first sheet
+                ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[sheetIndex];
+                DataTable dataTable = new DataTable("DataTable1");
+
+                // Load columns
+                foreach (var firstRowCell in worksheet.Cells[1, 1, 1, worksheet.Dimension.End.Column])
+                {
+                    dataTable.Columns.Add(firstRowCell.Text);
+                }
+
+                // Load rows
+                for (int rowNum = 2; rowNum <= worksheet.Dimension.End.Row; rowNum++)
+                {
+                    ExcelRange row = worksheet.Cells[rowNum, 1, rowNum, worksheet.Dimension.End.Column];
+                    DataRow newRow = dataTable.Rows.Add();
+                    foreach (var cell in row)
+                    {
+
+                        newRow[cell.Start.Column - 1] = cell.Text;
+                    }
+                }
+                
+
+                dataSet.Tables.Add(dataTable);
+                if (dataSet.Tables.Count > 0)
+                {
+                    dataGridView1.DataSource = dataSet.Tables[1];
+                }
+
+                //string connectionString = string.Format("provider=Microsoft.Jet.OLEDB.4.0; data source={0};Extended Properties=Excel 8.0;", filePath);
+                //string query = string.Format("SELECT * FROM [{0}$]", worksheet.Name);
+
+                //DataSet data = new DataSet();
+                //using (OleDbConnection con = new OleDbConnection(connectionString))
+                //{
+                //    con.Open();
+                //    OleDbDataAdapter adapter = new OleDbDataAdapter(query, con);
+                //    adapter.Fill(data);
+                //}
+
+
+
+                //////////////////////////////////////////////
+                ///
+
+                 reportViewer2 = new ReportViewer();
+                reportViewer2.ProcessingMode = ProcessingMode.Local;
+                reportViewer2.LocalReport.ReportPath = @"C:\\Users\\prathamesh_bhuvad\\Desktop\\VASP SOLUTIONS\\Excel_Utility\\Excel_Utility\\Excel_Utility\\rptJob.rdlc"; // Path to your RDLC report file
+
+                // Add your DataSet to the report
+                reportViewer2.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", dataSet.Tables[1])); // 'DataSet1' is the name of the dataset in your report
+
+                // Display the report
+                reportViewer2.Dock = DockStyle.Fill;
+                this.Controls.Add(reportViewer2);
+                reportViewer2.RefreshReport();
+
+
+
+
+
+
+
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void reportViewer2_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
